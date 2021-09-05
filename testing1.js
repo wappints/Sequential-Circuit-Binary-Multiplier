2cd70102f85824cf0886589bb6096d8a0789b21b
JMarie39 <juliana.marie39@gmail.com>
2021-09-05 23:19:59 +0800

    Added comments and TODO

--- testing1.js
+++ testing1.js
@@ -1,38 +1,53 @@
 // Sequential Circuit Binary Multiplier
+// TODO: Fix the compare() na, pag inadd hindi siya add lang since binary.
+// TODO: Add validator for MAX of 16-bit length yung multiplier and multiplicand.
+// TODO: Input: binary (use minimum number of bits; max up to 16-bit)
+// TODO: Output: Option to show either step-by-step or “all” mode of the A and Q output after every step with option to output result in TEXT FILE.
 
 // Initializations
-let M = "0100"; // Multiplicand
-let Q = "0101"; // Multiplier
-Q1 = "0";
-
-len = checkLength(); // Check the length of the multiplicand and multiplier.
+let M = "0100"; // Multiplicand // TODO: update this.
+let Q = "0101"; // Multiplier // TODO: update this.
+let Q1 = "0";
+let len = checkLength(); // Check the length of the multiplicand and multiplier.
 let A = getA(M); // Pads the zeros.
-negM = get2sComplement(M); // Gets the 2's complement of M.
+let negM = get2sComplement(M); // Gets the 2's complement of M.
 
+// TODO: For checking -- remove afterwards
+// ---------------------------------------------------------------------------------------
+console.log("XX-------------------------------------XX");
 print(negM, M, A, Q, Q1); // TODO: remove this afterwards
+console.log("XX-------------------------------------XX"); // TODO: remove this afterwards
 console.log(); // TODO: remove this afterwards
+// ---------------------------------------------------------------------------------------
 
 let tempString = A.concat(Q, Q1); // Concatinate
 
 program(len, tempString);
 
+// This function is used to compute for the sequential binary circuit.
+// Parameter: len, the length of the binaries; tempString, concatinated string.
 function program(len, tempString) {
   for (let i = 0; i < len; i++) {
+    console.log(i + 1 + ".)");
     tempString = compare(tempString);
-    console.log(tempString);
+    console.log("= " + tempString);
+    console.log("---------------------------------------");
   }
 }
 
 // TODO: For checking -- remove afterwards
+// ---------------------------------------------------------------------------------------
 function print(negM, M, A, Q, Q1) {
-  console.log("-M: " + negM);
-  console.log("M: " + M);
-  console.log("A: " + A + "  Q: " + Q + " Q-1: " + Q1);
+  console.log("  -M: " + negM);
+  console.log("  M: " + M);
+  console.log("  A: " + A + "  Q: " + Q + " Q-1: " + Q1);
   return true;
 }
+// ---------------------------------------------------------------------------------------
 
 // This function checks the length of the multiplicand and multiplier.
 // Otherwise, adjust it so that it will have the same length.
+// Returns: length of the binary.
 function checkLength() {
   let lenM = M.length;
   let lenQ = Q.length;
@@ -55,19 +70,31 @@ function checkLength() {
   return lenM > lenQ ? lenM : lenQ;
 }
 
+// This function is used to get A (zeros)
+// Parameter: M, the multiplicand.
+// Returns: The padded zeros with the same length as M & Q.
 function getA(M) {
   return padZero(0, M.length);
 }
 
+// This function is used to pad zeros.
+// Parameter: num, the number to be padded; length to be padded.
+// Returns: padded zeros.
 function padZero(num, digits) {
   let temp = parseInt(num);
   return Array(Math.max(digits - String(temp).length + 1, 0)).join(0) + temp;
 }
 
+// This function is used to pad ones.
+// Parameter: num, the number to be padded; length to be padded.
+// Returns: padded ones.
 function padOne(num, digits) {
   return Array(Math.max(digits - String(num).length + 1, 0)).join(1) + num;
 }
 
+// This function is used to replace a certain character in the string.
+// Parameter: str, string; index to be replaced, character to be replaced.
+// Returns: updated string.
 function replaceAtIndex(str, index, char) {
   console.log("String: " + str + "  Index: " + index + " Char:" + char);
   if (index > str.length - 1) {
@@ -77,6 +104,9 @@ function replaceAtIndex(str, index, char) {
   }
 }
 
+// This function is used to get the 2's complement of M.
+// Parameter: M, the multiplicand.
+// Returns: M.
 function get2sComplement(M) {
   let lenM = M.length;
   let isOne = true;
@@ -96,6 +126,9 @@ function get2sComplement(M) {
 }
 
 // TODO: Fix the values when adding the binaries.
+// This function checks the last digit of Q and Q-1
+// Parameter: tempString, the concatinated string.
+// Returns: tempString.
 function compare(tempString) {
   let _len = tempString.length;
 
@@ -107,6 +140,7 @@ function compare(tempString) {
     // if 1 & 0
     else {
       for (let i = 0; i < A.length; i++) {
+        console.log("A-M");
         tempString = replaceAtIndex(
           tempString,
           i,
@@ -122,6 +156,7 @@ function compare(tempString) {
     // if 0 & 1
     else {
       for (let i = 0; i < A.length; i++) {
+        console.log("A+M");
         tempString = replaceAtIndex(
           tempString,
           i,
@@ -130,10 +165,12 @@ function compare(tempString) {
       }
     }
   }
-  let SARstring = SAR(tempString);
-  return SARstring;
+  return SAR(tempString);
 }
 
+// This function pertains to shift arithmetic right.
+// Parameter: tempString, the concatinated string.
+// Returns: tempString.
 function SAR(tempString) {
   tempString = tempString[0].concat(tempString);
   tempString = tempString.slice(0, -1);
